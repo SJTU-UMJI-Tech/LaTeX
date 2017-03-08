13a7065ce13bd14fef8b7f2d1f51a8fdf066d248
tc-imba <liuyh615@126.com>
2017-03-08 17:18:14 +0800

    package.json

---
+++ README.md
@@ -0,0 +1,7 @@
+# LaTeX Lecture
+
+This is a beamer slide for LaTeX lecture in JI
+
+## Compiling
+
+run `npm start` to generate pdf files (you need to install node and LaTeX on your system)
---
+++ build.js
@@ -0,0 +1,19 @@
+/**
+ * Created by liu on 17-3-8.
+ */
+const util = require("util"),
+    latex = require("gammalatex"),
+    fs = require("fs");
+
+console.log('Reading Tex Files ...');
+let string = fs.readFileSync('lecture.tex', 'utf-8');
+
+console.log('Compiling into pdf ...');
+
+latex.parse(string, function (err, readStream) {
+    if (err) throw err;
+    //console.log(readStream);
+    const writeStream = fs.createWriteStream("lecture.pdf");
+    readStream.pipe(writeStream);
+    console.log('pdf file generated.');
+});
